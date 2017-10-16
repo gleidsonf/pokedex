8b65b322e47023fcd6b9325c593d0ad2c7a186c8
Gleidson Silva <gleidson.fgs@gmail.com>
2017-10-16 16:41:00 -0300

    Carregando variaveis no html

--- www/js/index.js
+++ www/js/index.js
@@ -1,35 +1,60 @@
-$.getJSON("https://pokeapi.co/api/v2/pokemon/?limit=151", function(data){
-  lista = $("#pokemons");
-  data.results.forEach(function(pokemon){
-    var li = $('<li></li>');
-    li.text(pokemon.name);
-      lista.append(li);
-      console.log(pokemon);
-  });
-});
+// $.getJSON("https://pokeapi.co/api/v2/pokemon/?limit=151", function(data){
+//   lista = $("#pokemons");
+//   data.results.forEach(function(pokemon){
+//     var li = $('<li></li>');
+//     li.text(pokemon.name);
+//       lista.append(li);
+//       console.log(pokemon);
+//   });
+// });
 
-var pokemon;
-$.getJSON("https://pokeapi.co/api/v2/pokemon/1/", function(data) {
-  pokemon = data;
-  console.log(data);
-});
+
+// var pokemon;
+// $.getJSON("https://pokeapi.co/api/v2/pokemon/1/", function(data) {
+//   pokemon = data;
+//   console.log(data);
+// });
+
 // pokemon.name, pokemon.abilities[0].ability.name, pokemon.height, pokemon.weight,
 // pokemon.types[0].type.name, pokemon.stats[0](Status base, valor de atk e def)
 // pokemon.sprites
 
-
+var pokemon;
 $.ajax({
 	url: 'https://pokeapi.co/api/v2/pokemon/1',
 	success: function(data) {
 		console.log('Funciona');
-		$('.name').text(data.name);
-		$('.sprite').attr('src', data.sprites.front_default);
+    $('.sprite').attr('src', data.sprites.front_default);
+    $('.id-pokemon').text(data.id);
+    $('.name').text(data.name);
+      data.types.forEach(function(type) {
+        var tag = $("<span></span>").text(type.type.name+" ");   // Create with jQuery
+        $('.type').append(tag);
+      });
+      $('.height').text(data.height);
+      $('.weight').text(data.weight);
+      pokemon = data;
+      returnData(data.species.url);
 	},
 	error: function(err){
 		console.log('nao funciona');
 	}
-
 });
+
+// https://pokeapi.co/api/v2/pokemon-species/1/
+// https://pokeapi.co/api/v1/pokemon/1/
+function returnData(url_data) {
+  $.ajax({
+  	url: url_data,
+  	success: function(data) {
+      $('.description').text(data.flavor_text_entries[1].flavor_text)
+      console.log(data.habitat.name)
+  	},
+  	error: function(err){
+  		console.log('nao funciona');
+  	}
+  });
+}
 
 var detalhe;
 $.getJSON(pokemon.species.url, function(data) {detalhe = data;});
